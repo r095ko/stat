1776cca285a961ddcdbe96af046aaaf79ef091dd
r095ko <r095ko@gmail.com>
2022-04-05 17:33:28 +0300

    fix

--- src/test/java/ru/netology/stats/statServiceTest.java
+++ src/test/java/ru/netology/stats/statServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.stats;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatServiceTest {
     @Test
